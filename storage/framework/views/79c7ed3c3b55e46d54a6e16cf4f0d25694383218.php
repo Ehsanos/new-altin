7f9ff757374a894b4d08c3018b716f7ffdba2aa5
Ehsano Gamor <ehsan.gamor90@gmail.com>
2024-03-10 13:18:47 +0300

    add search in top nav2

--- storage/framework/views/79c7ed3c3b55e46d54a6e16cf4f0d25694383218.php
+++ storage/framework/views/79c7ed3c3b55e46d54a6e16cf4f0d25694383218.php
@@ -53,8 +53,9 @@
 
     <section class="d-flex flex-column justify-content-center align-items-center products-1 py-2"<?php if($style): ?> style="background-color:<?php echo e($style->primary); ?>" <?php endif; ?> >
         <div class="container-fluid" >
-            <div class="row">
-                <div class="col-12 col-lg-2">
+            <i class="fas fa-folder-plus open-cats d-lg-none mt-2 " id="open-cats"></i>
+            <div class="row ">
+                <div class="col-12 col-lg-2 show-cats" id="cats-div">
                     <div class="row">
                         <div class="col-12 m-0">
                             <div
@@ -82,7 +83,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="row pt-5">
+                <div class="row pt-5 ">
                     <div class="col-12 alfeat-head">
                         <div class="p-2">
                             <h3 class="text-dark font-weight-bolder m-0"><?php echo e(lang('cats')); ?></h3>
@@ -116,6 +117,7 @@
                     </div>
                 </div>
             </div>
+
             <div class="col-12 col-lg-10">
                 <div class="row p-3">
                     <div class="col p-0">
@@ -193,6 +195,7 @@
                     <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                 </div>
             </div>
+
         </div>
 
 
