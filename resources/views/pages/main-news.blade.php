ad9c6eef8d61bf707cc060b97ef2c1c771e6443b
Ehsano Gamor <ehsan.gamor90@gmail.com>
2024-03-10 16:48:35 +0300

    news page

--- resources/views/pages/main-news.blade.php
+++ resources/views/pages/main-news.blade.php
@@ -51,17 +51,31 @@
         <section class="d-flex flex-column justify-content-center align-items-center products-1 py-2">
             <div class="container-fluid py-5">
                 <div class="row justify-content-center">
-                    @foreach($posts as $post)
-                        <div class="col-12 col-lg-4 mb-lg-0" style="padding: 10px;"><a
-                                href="{{route('langs.showPost',['post'=>$post])}}">
-                                <div class="hover hover-2 rounded" style="color: #ffffff;"><img class="img-fluid"
-                                                                                                src="{{$post->getFirstMediaUrl('posts')}}"
-                                                                                                alt="image">
-                                    <div class="hover-overlay"></div>
-                                    <div class="hover-1-content px-5 py-4">
-                                        <span class="text-uppercase hover-2-title mb-0"
-                                            style="font-weight: bold;">{!!getTrans($post,'tilte')!!}</span>
+                    @foreach($posts as $new)
+
+{{--                        <div class="col-12 col-lg-4 mb-lg-0" style="padding: 10px;"><a--}}
+{{--                                href="{{route('langs.showPost',['post'=>$post])}}">--}}
+{{--                                <div class="hover hover-2 rounded" style="color: #ffffff;"><img class="img-fluid"--}}
+{{--                                                                                                src="{{$post->getFirstMediaUrl('posts')}}"--}}
+{{--                                                                                                alt="image">--}}
+{{--                                    <div class="hover-overlay"></div>--}}
+{{--                                    <div class="hover-1-content px-5 py-4">--}}
+{{--                                        <span class="text-uppercase hover-2-title mb-0"--}}
+{{--                                            style="font-weight: bold;">{!!getTrans($post,'tilte')!!}</span>--}}
 {{--                                        <p class="hover-2-description font-weight-light mb-0">{!!getTrans($post,'body')!!}</p>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                            </a></div>--}}
+                        <div class="col-3"><a class="text-decoration-none"
+                                                          href="{{route('langs.showPost',['post'=>$new])}}">
+                                <div class="card cards-shadown cards-hover w-100" data-aos="flip-left"
+                                     data-aos-duration="950">
+                                    <div class="card-header"><img class="img-fluid rounded-img"
+                                                                  src="{{$new->getFirstMediaUrl('posts')}}">
+                                    </div>
+                                    <div class="card-body">
+                                        <p class="card-text sub-text-color">{!!getTrans($new,'tilte')!!}</p>
+                                        {{--                                                <p class="card-text cardbody-sub-text">{{getTrans($new,'body')}}</p>--}}
                                     </div>
                                 </div>
                             </a></div>
--- storage/framework/views/3651451e898aca40efd14066cd7bb6a540c811e2.php
+++ storage/framework/views/3651451e898aca40efd14066cd7bb6a540c811e2.php
@@ -50,17 +50,31 @@
         <section class="d-flex flex-column justify-content-center align-items-center products-1 py-2">
             <div class="container-fluid py-5">
                 <div class="row justify-content-center">
-                    <?php $__currentLoopData = $posts; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $post): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-                        <div class="col-12 col-lg-4 mb-lg-0" style="padding: 10px;"><a
-                                href="<?php echo e(route('langs.showPost',['post'=>$post])); ?>">
-                                <div class="hover hover-2 rounded" style="color: #ffffff;"><img class="img-fluid"
-                                                                                                src="<?php echo e($post->getFirstMediaUrl('posts')); ?>"
-                                                                                                alt="image">
-                                    <div class="hover-overlay"></div>
-                                    <div class="hover-1-content px-5 py-4">
-                                        <span class="text-uppercase hover-2-title mb-0"
-                                            style="font-weight: bold;"><?php echo getTrans($post,'tilte'); ?></span>
+                    <?php $__currentLoopData = $posts; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $new): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+                        <div class="col-3"><a class="text-decoration-none"
+                                                          href="<?php echo e(route('langs.showPost',['post'=>$new])); ?>">
+                                <div class="card cards-shadown cards-hover w-100" data-aos="flip-left"
+                                     data-aos-duration="950">
+                                    <div class="card-header"><img class="img-fluid rounded-img"
+                                                                  src="<?php echo e($new->getFirstMediaUrl('posts')); ?>">
+                                    </div>
+                                    <div class="card-body">
+                                        <p class="card-text sub-text-color"><?php echo getTrans($new,'tilte'); ?></p>
+                                        
                                     </div>
                                 </div>
                             </a></div>
