82c00c7fa3ad635b5bdeb5a051111ec118aaef2e
Ehsano Gamor <ehsan.gamor90@gmail.com>
2024-03-12 15:35:43 +0300

    fix design

--- resources/views/pages/products-in-cat2.blade.php
+++ resources/views/pages/products-in-cat2.blade.php
@@ -8,9 +8,13 @@
                 <div class="alert alert-success">{{ Session::get('message') }}</div>
             </div>
         @endif
-        <div class="container" >
+        <div class="container-fluid" >
+            <i class="fas fa-folder-plus open-cats d-lg-none mt-2 " id="open-cats">
+                <span class="words-cat">{{lang('cats').' + '.lang('sections')}}</span>
+            </i>
+
             <div class="row">
-                <div class="col-12 col-lg-3">
+                <div class="col-12 col-lg-3 show-cats" id="cats-div">
                     <div class="row">
                         <div class="col-12 m-0">
                             <div
@@ -100,7 +104,7 @@
                         </div>
 
 
-                        <div class="col-12 col-md-6 col-lg-4 mb-2" onmouseover="show(this)" onmouseleave="hide(this)">
+                        <div class="col-6 col-md-6 col-lg-4 mb-2" onmouseover="show(this)" onmouseleave="hide(this)">
                             <div class="adding-hidden" id="add">
                                 <form action="{{route('langs.addToCart')}}" method="post">
                                     @csrf
