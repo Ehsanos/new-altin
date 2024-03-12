d485ca758b85e8e30de734252551fb1e989108fa
Ehsano Gamor <ehsan.gamor90@gmail.com>
2024-03-12 15:36:11 +0300

    fix design

--- storage/framework/views/268ac35926748eb5e10d7e4be59b9ab12bf11c64.php
+++ storage/framework/views/268ac35926748eb5e10d7e4be59b9ab12bf11c64.php
@@ -7,9 +7,13 @@
                 <div class="alert alert-success"><?php echo e(Session::get('message')); ?></div>
             </div>
         <?php endif; ?>
-        <div class="container" >
+        <div class="container-fluid" >
+            <i class="fas fa-folder-plus open-cats d-lg-none mt-2 " id="open-cats">
+                <span class="words-cat"><?php echo e(lang('cats').' + '.lang('sections')); ?></span>
+            </i>
+
             <div class="row">
-                <div class="col-12 col-lg-3">
+                <div class="col-12 col-lg-3 show-cats" id="cats-div">
                     <div class="row">
                         <div class="col-12 m-0">
                             <div
@@ -62,7 +66,8 @@
                             <div class="list-group">
                                 <?php $__currentLoopData = $departments; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $dep): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                                     <a class="list-group-item list-group-item-action font-weight-bolder"
-                                       href="<?php echo e(route("langs.products",["catId"=>$dep->category_id,"depId"=>"$dep->id"])); ?>"><?php echo e($dep->name); ?></a>
+                                       href="<?php echo e(route("langs.products",["catId"=>$dep->category_id,"depId"=>"$dep->id"])); ?>">
+                                        <?php echo e(getTrans($dep,'name')); ?></a>
                                 <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 
                             </div>
@@ -98,7 +103,7 @@
                         </div>
 
 
-                        <div class="col-12 col-md-6 col-lg-4 mb-2" onmouseover="show(this)" onmouseleave="hide(this)">
+                        <div class="col-6 col-md-6 col-lg-4 mb-2" onmouseover="show(this)" onmouseleave="hide(this)">
                             <div class="adding-hidden" id="add">
                                 <form action="<?php echo e(route('langs.addToCart')); ?>" method="post">
                                     <?php echo csrf_field(); ?>
@@ -116,7 +121,7 @@
                             <a class="text-decoration-none" href="<?php echo e(route('langs.product_details',[$product])); ?>">
                                 <div class="p-2 card product-main">
                                     <div class="text-center">
-                                        <h5 class="text-truncate font-weight-bolder"><?php echo e(getTrans($product,'name')); ?></h5>
+                                        <h5 class="text-truncate font-weight-bolder"><?php echo e($product->code ?? 'code'); ?></h5>
                                     </div>
                                     <div class="div-hr-w"></div>
                                     <div>
@@ -124,8 +129,24 @@
                                                                                    src="<?php echo e($product->getFirstMediaUrl('products')); ?>">
                                         </div>
                                         <div class="px-3">
-                                            <p class="text-dark font-weight-bold"><?php echo e($product->department->name ?? 'None'); ?></p>
+                                            <p class="text-dark font-weight-bold"><?php echo e(getTrans($product,'name')
+                                         ??
+                                        'None'); ?></p>
+                                            <?php if(app()->getLocale()=='ar'): ?>
+                                                <p class="text-dark font-weight-bold"><?php echo e($product->department->name  ??
+                                        'None'); ?></p>
+                                            <?php elseif(app()->getLocale()=='en'): ?>
+                                                <p class="text-dark font-weight-bold"><?php echo e($product->department->name_en  ??
+                                        'None'); ?></p>
+
+                                            <?php elseif(app()->getLocale()=='tr'): ?>
+                                                <p class="text-dark font-weight-bold"><?php echo e($product->department->name_tr  ??
+                                        'None'); ?></p>
+
+                                            <?php endif; ?>
+
                                         </div>
+
                                     </div>
                                 </div>
                             </a>
